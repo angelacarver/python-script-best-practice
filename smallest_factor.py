352c09cef2575034043ec6ad6fe00370237c41f9
Angela Carver <acc0061@venus.auburn.edu>
2021-03-26 10:14:00 -0500

    Finished the exercise

--- smallest_factor.py
+++ smallest_factor.py
@@ -5,6 +5,31 @@
 import sys
 
 def get_smallest_prime_factor(n):
+    """
+    Returns the smallest integer that is a factor of `n`.
+
+    If `n` is a prime number `None` is returned.
+    
+    Parameters
+    ----------
+    n : int
+        The integer to be factored.
+
+    Returns
+    -------
+    int or None
+        The smallest integer that is a factor of `n`
+        or None if `n` is a prime.
+
+    Examples
+    --------
+    >>> get_smallest_prime_factor(7)
+    >>> get_smallest_prime_factor(8)
+    2
+    >>> get_smallest_prime_factor(9)
+    3
+    """
+
     for i in range(2, n):
         if (n % i) == 0:
             return i
